bfee27ce3803e956acbde0d20b65462eba2ea499
Franzen Hernandez <your_github_email@example.com>
2026-04-16 15:52:29 +0800

    update

--- Ecommerce_Souvenir_App/app/(tabs)/profile.tsx
+++ Ecommerce_Souvenir_App/app/(tabs)/profile.tsx
@@ -10,7 +10,7 @@ import api from "../../config/api";
 import * as ImagePicker from "expo-image-picker";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://192.168.1.30:5000";
+const SOCKET_URL = "http://192.168.193.222:5000";
 
 interface OrderItem {
   id: string;
--- Ecommerce_Souvenir_App/config/api.js
+++ Ecommerce_Souvenir_App/config/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.30:5000/api', // ✅ updated!
+  baseURL: 'http://192.168.193.222:5000/api',
+  timeout: 10000,
 });
 
 api.interceptors.request.use(async (config) => {
@@ -10,5 +11,17 @@ api.interceptors.request.use(async (config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.code === 'ECONNABORTED') {
+      error.message = 'Connection timed out. Check if the backend server is running.';
+    } else if (!error.response) {
+      error.message = 'Cannot connect to server. Check your IP address and WiFi connection.';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
